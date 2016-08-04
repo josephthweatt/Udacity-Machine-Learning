02bdf3c208dd0ad167de53050507cc356e312d4e
joseph <josephthweatt@gmail.com>
2016-08-04 15:39:32 -0700

    Created a folder for a personal project, which fits a model to the s&p 500

---
+++ Personal Projects/PolyfitSP.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+#get data from s&p 500, then fit a model to it, and then print as graph
+
+#extract data
+import pandas as pd
+in_file = "table.csv"
+all_data = pd.read_csv(in_file)
+
+import numpy as np
+import matplotlib.dates as dates 
+import matplotlib.pyplot as plt
+dates = map(lambda x: dates.datestr2num(x), all_data['Date'].tolist())
+closes = map(lambda x: int(x), all_data['Close'].tolist())
+coeffs = np.polyfit(dates, closes, 3)
+function = np.poly1d(coeffs)
+plt.plot(dates, function(dates))
+plt.plot(dates, closes)
+plt.show()
